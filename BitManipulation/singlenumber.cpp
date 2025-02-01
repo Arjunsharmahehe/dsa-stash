68975cca36547ad87ada4cafa524856f260cf797
arjunsharma <whocaresaboutarjun@gmail.com>
2025-02-01 18:34:21 +0530

    solve xor problem

---
+++ BitManipulation/singlenumber.cpp
@@ -0,0 +1,18 @@
+
+
+int singleNumber(vector<int>& nums) {
+
+    // Checking if the length of the array is 1
+    if(nums.size() < 2) return nums[0];
+
+    // XOR return 0 when we XOR the same numbers
+    int XOR;
+    XOR = nums[0];
+    for(int i = 1; i < nums.size(); ++i){
+        XOR = XOR^nums[i];
+    }
+
+    // The resultant value of XOR is the number that appears odd number of times
+    return XOR;
+
+}
